796c215923670cb0e2fb3f363c544d010b3dcd1a
qiuyantang <395980378@qq.com>
2025-02-08 11:16:09 +0800

    添加两个测试类

---
+++ moduleA/src/main/java/com/example/modulea/TestAUtils.kt
@@ -0,0 +1,11 @@
+package com.example.modulea
+
+import android.util.Log
+
+class TestAUtils {
+    companion object{
+        fun test(){
+            Log.d("======","TestAUtils")
+        }
+    }
+}
---
+++ moduleB/src/main/java/com/example/moduleb/TestBUtils.kt
@@ -0,0 +1,11 @@
+package com.example.moduleb
+
+import android.util.Log
+
+class TestBUtils {
+    companion object{
+        fun test(){
+            Log.d("======","TestBUtils")
+        }
+    }
+}
